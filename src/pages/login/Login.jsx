e1b31530c48c814de992732fd0b0d112eecbb48e
Angel Rodriguez <53921920+angelz510@users.noreply.github.com>
2023-10-03 16:33:24 -0700

    styled a login page cute

---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,26 @@
+import "./login.scss";
+
+const login = () => {
+  return (
+    <div className="login">
+      <div className="card">
+        <div className="left">
+          <h1>Welcome.</h1>
+          <p>Because our pets have their own friends.</p>
+          <span>Dont have an account?</span>
+          <button>Register</button>
+        </div>
+        <div className="right">
+          <h1>Login</h1>
+          <form>
+            <input type="text" placeholder="Username" />
+            <input type="text" placeholder="Password" />
+            <button>Login</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default login;
