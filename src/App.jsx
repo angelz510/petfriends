102060bfabade48ca4124ffb3397cafc7b3b1dfc
Angel Rodriguez <53921920+angelz510@users.noreply.github.com>
2023-10-10 16:59:33 -0700

    add home and profile routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import "./app.scss";
 import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
+import Home from "./pages/home/Home";
+import Profile from "./pages/profile/Profile";
 import Navbar from "./components/navbar/Navbar";
 import Leftbar from "./components/leftbar/Leftbar";
 import Rightbar from "./components/rightbar/Rightbar";
@@ -24,7 +26,16 @@ function App() {
     {
       path: "/",
       element: <Layout />,
-      children: [{}],
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/profile/:id",
+          element: <Profile />,
+        },
+      ],
     },
     {
       path: "/login",
