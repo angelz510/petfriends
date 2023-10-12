be22d95f0c9f8ecdb6a935372239db15274ce665
Angel Rodriguez <53921920+angelz510@users.noreply.github.com>
2023-10-12 16:56:22 -0700

    style right bar

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -1,7 +1,25 @@
 import "./rightbar.scss";
 
 const Rightbar = () => {
-  return <div className="rightbar">Rightbar</div>;
+  return (
+    <div className="rightbar">
+      <div className="container">
+        <div className="item">
+          <span>Suggestions For You</span>
+          <div className="user">
+            <div className="user-info">
+              <img src="https://i.imgur.com/hUPONVM.jpg" alt="" />
+              <span>Jose Perez</span>
+            </div>
+            <div className="buttons">
+              <button>follow</button>
+              <button>unfollow</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Rightbar;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,37 @@
 import "./home.scss";
 
 const Home = () => {
-  return <div className="home">Home</div>;
+  return (
+    <div className="home">
+      <h1>HOME</h1>
+      <h1>HOME</h1>
+      <h1>HOME</h1>
+      <h1>HOME</h1>
+      <h1>HOME</h1>
+      <h1>HOME</h1>
+      <h1>HOME</h1>
+      <h1>HOME</h1>
+      <h1>HOME</h1>
+      <h1>HOME</h1>
+      <h1>HOME</h1>
+      <h1>HOME</h1>
+      <h1>HOME</h1>
+      <h1>HOME</h1>
+      <h1>HOME</h1>
+      <h1>HOME</h1>
+      <h1>HOME</h1>
+      <h1>HOME</h1>
+      <h1>HOME</h1>
+      <h1>HOME</h1>
+      <h1>HOME</h1>
+      <h1>HOME</h1>
+      <h1>HOME</h1>
+      <h1>HOME</h1>
+      <h1>HOME</h1>
+      <h1>HOME</h1>
+      <h1>HOME</h1>
+    </div>
+  );
 };
 
 export default Home;
